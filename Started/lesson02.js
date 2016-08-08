91ede9a2dc632ea6c6c89fa5caf4b278d2aa38d6
tdongsi <dongsi.tuecuong@gmail.com>
2016-08-07 21:58:07 -0700

    Practice: Variable scope.

--- Started/lesson02.js
+++ Started/lesson02.js
@@ -1,10 +1,15 @@
-var rollDice = function() {
-	return 1 + Math.floor(6*Math.random());
+var rollDice = function(diceSize) {
+	var result = 1 + Math.floor(diceSize*Math.random())
+	return result;
 };
 
-var firstDice = rollDice();
-var secondDice = rollDice();
+var showResult = function() {
+	console.log(firstDice);
+	console.log(secondDice);
+	console.log(firstDice + secondDice);
+};
 
-console.log(firstDice);
-console.log(secondDice);
-console.log(firstDice + secondDice);
+// Global variables
+var firstDice = rollDice(6);
+var secondDice = rollDice(6);
+showResult();
