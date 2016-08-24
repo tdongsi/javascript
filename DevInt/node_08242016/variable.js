655c9e073384b9efeea1d7f60f2f9ba92928c52b
tdongsi <dongsi.tuecuong@gmail.com>
2016-08-24 13:46:00 -0700

    Practice: Closure.

--- DevInt/node_08242016/variable.js
+++ DevInt/node_08242016/variable.js
@@ -21,3 +21,17 @@ const doIt2 = function() {
 const doIt3 = () => {
     console.log('doIt3')
 }
+
+/*
+Which function declaration should be used?
+Most of the time, use arrow expression.
+Use function expression when you definitely want protoype inheritance.
+*/
+
+// IIFE - immediately invoked function expression
+// Restrict scope t into local variable scope, instead of global scope when outside function.
+(function() {
+    var t = 10;
+    console.log(t);
+}) ();
+
