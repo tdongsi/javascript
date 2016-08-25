4cb1c9bf21d6c08f76a63d1a3d291d194034b066
tdongsi <dongsi.tuecuong@gmail.com>
2016-08-25 14:19:44 -0700

    Practice: File-based web server.

--- DevInt/rest/index.js
+++ DevInt/rest/index.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer(function (req, res) {
-    res.end("Hello World!");
+    fs.readFile('./www/index.html', 'utf8', function(err, data) {
+        if (err) {
+            res.end('could not find file');
+            return;
+        }
+
+        res.end(data);
+    })
 });
 
 server.listen(3000, function() {
