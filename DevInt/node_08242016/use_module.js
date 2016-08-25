04429f9d7bffce5031df16def53768f3ae7a97ed
tdongsi <dongsi.tuecuong@gmail.com>
2016-08-25 10:37:02 -0700

    Practice: More node-inspector simulation.

--- DevInt/node_08242016/use_module.js
+++ DevInt/node_08242016/use_module.js
@@ -10,16 +10,20 @@ myModule.doIt("This is fun");
 // simulate what node-inspector does
 function myRequire2(fileName) {
     var moduleCode = fs.readFileSync(fileName + '.js', 'utf8');
+
     var exports = {};
+    var module = {
+        exports: exports
+    };
 
     // This "generated" is similarly constructed by node-inspector
     // Here, we only implement the first input: exports
-    // In node-inspector, it takes 5 input: exports, require, module, __file, __dir
-    var generated = `(function(exports) { ${moduleCode} })`;
+    // In node-inspector, it takes 5 inputs: exports, require, module, __file, __dir
+    var generated = `(function(exports, require, module) { ${moduleCode} })`;
     console.log(generated);
     var moduleFn = eval(generated);
-    moduleFn(exports);
-    return exports;
+    moduleFn(exports, null, module);
+    return module.exports;
 }
 
 var temp = myRequire2("./module");
