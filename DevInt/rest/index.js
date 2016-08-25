dee89f8b2b29aec22319c367a6c9cfae8da3f3ad
tdongsi <dongsi.tuecuong@gmail.com>
2016-08-25 15:15:54 -0700

    Practice: Function object that is similar to express.static.

--- DevInt/rest/index.js
+++ DevInt/rest/index.js
@@ -6,10 +6,21 @@ const express = require('express');
 
 const app = express();
 console.log(typeof app); // should return function
-app.use(express.static('www')); // configure function object
+// app.use(express.static('www')); // configure function object
+
+app.use(function(req, res) {
+    fs.readFile('www/index.html', 'utf8', (err, data) => {
+        if (err) {
+            res.end("error");
+            return;
+        }
+        res.end(data);
+    });
+});
 
 const server = http.createServer(app);
 
 server.listen(3000, function() {
     console.log("listening on port 3000");
-})
+});
+
