e95b224dcdac61f3b6a90f0707a10bd6b7afc8e8
tdongsi <dongsi.tuecuong@gmail.com>
2016-08-25 15:56:51 -0700

    Practice: More dabbling with Express.

--- DevInt/rest/index.js
+++ DevInt/rest/index.js
@@ -8,7 +8,20 @@ const app = express();
 console.log(typeof app); // should return function
 // app.use(express.static('www')); // configure function object
 
+
+app.use("/duy", function(req, res, next) {
+    console.log("duy is cool");
+
+    // Use property to pass data to next function
+    req.passdata = "New data"
+
+    next();
+})
+
 app.use(function(req, res) {
+    console.log(req.url);
+    console.log(req.passdata);
+
     fs.readFile('www/index.html', 'utf8', (err, data) => {
         if (err) {
             res.end("error");
