c374c7ddc806033bcc4556d3103db0605442d252
Tue-Cuong Dong-Si <tdongsi@salesforce.com>
2017-02-11 16:36:43 -0800

    Grunt: Use grunt-connect to start up a local server.

---
+++ learn_grunt/grunt/tasks/connect.js
@@ -0,0 +1,13 @@
+module.exports = {
+    server: {
+        options: {
+            port: 9001,
+            protocol: "http",
+            hostname: "localhost",
+            base: ".",
+            directory: null,
+            open: true,
+            keepalive: true
+        }
+    }
+};
