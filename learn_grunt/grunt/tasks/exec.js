5f486ff6917ac8c637b128bdc3e781b07d80ace6
tdongsi <dongsi.tuecuong@gmail.com>
2017-02-12 00:01:42 -0800

    Grunt: Rewriting target but hit a knowledge limit.

--- learn_grunt/grunt/tasks/exec.js
+++ learn_grunt/grunt/tasks/exec.js
@@ -1,6 +1,14 @@
 module.exports = {
     github: {
-        command: "ssh -T git@github.com",
+        command: function() {
+            var command = "ssh -T";
+            // TODO: how to refer to github.options.verbose?
+            if (grunt.config.exec.github.options.verbose) {
+                command += " -v";
+            }
+            command += " git@github.com";
+            return command;
+        },
         exitCode: 1,
         stdout: false,
         options: {
