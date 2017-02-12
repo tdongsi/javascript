71b3e3bccbcab7443f953070c4cab181a74c12aa
tdongsi <dongsi.tuecuong@gmail.com>
2017-02-11 23:11:48 -0800

    Grunt: Install plugins for Qunit and an example Qunit test.

---
+++ learn_grunt/grunt/tasks/qunit.js
@@ -0,0 +1,9 @@
+module.exports = {
+    all: {
+        options: {
+            urls: [
+                'http://grunt.dev/tests/index.html'
+            ]
+        }
+    }
+}
---
+++ learn_grunt/test/tests.js
@@ -0,0 +1,3 @@
+QUnit.test( "hello test", funciton(assert) {
+    assert.ok( 1 == "1", "Passed!");
+})
